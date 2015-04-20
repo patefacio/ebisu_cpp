7b951d25703863c9fae6c270689ff716f8e0c279
Daniel Davidson <dbdavidson@yahoo.com>
2015-04-19 21:28:37 -0500

    bump version of id started on example TestScenario test

--- test/test_cpp_test_provider.dart
+++ test/test_cpp_test_provider.dart
@@ -18,11 +18,24 @@ final _logger = new Logger('test_cpp_test_provider');
 main([List<String> args]) {
   Logger.root.onRecord.listen(
       (LogRecord r) => print("${r.loggerName} [${r.level}]:\t${r.message}"));
-  Logger.root.level = Level.OFF;
+  Logger.root.level = Level.INFO;
 // custom <main>
 
+  vectorSample() => testScenario('basics',
+      given('a vector with some items', [
+        when('the size is increased',
+            then('the size and capacity change')),
+        when('the size is reduced',
+            then('the size channges but not capacity')),
+        when('more capacity is reserved',
+            then('the capacity changes but not the size')),
+        when('less capacity is reserved',
+            then('neither size nore capacity is changed')),
+      ]));
+
   test('test_scenario', () {
-    _logger.info(testScenario('foo_bar'));
+    final sampleTestScenario = vectorSample();
+    _logger.info(sampleTestScenario);
   });
 
 // end <main>
