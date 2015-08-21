db9f6cef611987a4f69937292a68b95cced9d2de
Daniel Davidson <dbdavidson@yahoo.com>
2015-08-20 21:28:50 -0500

    bump version with fixed Class(id) behavior

--- codegen/ebisu_cpp.ebisu.dart
+++ codegen/ebisu_cpp.ebisu.dart
@@ -28,7 +28,7 @@ files, build scripts, test files, etc.)
     ..includesHop = true
     ..license = 'boost'
     ..pubSpec.homepage = 'https://github.com/patefacio/ebisu_cpp'
-    ..pubSpec.version = '0.3.4'
+    ..pubSpec.version = '0.3.5'
     ..pubSpec.doc = purpose
     ..rootPath = _topDir
     ..doc = purpose
