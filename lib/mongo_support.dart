eb88204121a7d59fceab0f3c968e1317a462de91
Daniel Davidson <dbdavidson@yahoo.com>
2015-09-29 06:51:42 -0500

    stub for mongo support

---
+++ lib/mongo_support.dart
@@ -0,0 +1,11 @@
+library ebisu_cpp.mongo_support;
+
+import 'package:ebisu/ebisu.dart';
+import 'package:ebisu_cpp/ebisu_cpp.dart';
+import 'package:id/id.dart';
+
+// custom <additional imports>
+// end <additional imports>
+
+// custom <library mongo_support>
+// end <library mongo_support>
